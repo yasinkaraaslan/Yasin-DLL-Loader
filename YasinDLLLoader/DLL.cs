e1be7b3b47eeb8e94a81297a2ef2597d4fee7c62
Yasin Karaaslan <yasink.aslan51@gmail.com>
2021-06-11 19:00:01 +0300

    Add project files.

---
+++ YasinDLLLoader/DLL.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace YasinDLLLoader
+{
+    class DLL
+    {
+        public static string Directory; // Directory where dlls are stored
+
+        public object instance { get; private set; }
+        public Type classType { get; private set; }
+
+
+        public string name { get; private set; }
+
+        public bool isValid { get; private set; } = true;
+        
+        public string className => Path.GetFileNameWithoutExtension(name);
+
+
+        public DLL(string dllPath)
+        {
+
+            try
+            {
+                name = Path.GetFileName(dllPath);
+                Assembly assembly = Assembly.LoadFile(dllPath);
+                if (assembly == null)
+                {
+                    isValid = false;
+                    return;
+                }
+
+                object obj = assembly.CreateInstance(className);
+                if (obj == null)
+                {
+                    DLLLoaderSubModule.ShowMessageBox("Make sure the class name is same as dll name. " + name, MessageBoxIcon.Error);
+                    isValid = false;
+                    return;
+                }
+                instance = obj;
+                classType = instance.GetType();
+            }
+            catch (FileLoadException)
+            {
+                isValid = false;
+                DLLLoaderSubModule.SendMessage(name + " has already been loaded.");
+            } // The Assembly has already been loaded.
+            catch (BadImageFormatException)
+            {
+                isValid = false;
+                string msg = name + " couldn't be loaded! Make sure the file is a .NET assembly.";
+                DLLLoaderSubModule.ShowMessageBox(msg, MessageBoxIcon.Error);
+            } // If a BadImageFormatException exception is thrown, the file is not an assembly.
+            catch (Exception e)
+            {
+                isValid = false;
+                DLLLoaderSubModule.ShowMessageBox(e.Message + " in " + name, MessageBoxIcon.Error);
+            } // Other exceptions
+
+        }
+
+        // TODO: Add support for parameters
+        public void InvokeMethod(string methodName, BindingFlags bindingFlags, object[] parameters)
+        {
+            MethodInfo mi = classType.GetMethod(methodName, bindingFlags);
+            if (mi != null)
+            {
+                mi.Invoke(instance, parameters);
+            }
+        }
+
+    }
+}
---
+++ YasinDLLLoader/DLLLoaderSubModule.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+
+namespace YasinDLLLoader
+{
+    public class DLLLoaderSubModule : MBSubModuleBase
+    {
+        List<DLL> dlls = new List<DLL>();
+
+        // These strings may be customizable in the future
+        const string ModuleLoadMethod = "BannerlordModuleLoad";
+        const string GameStartMethod = "BannerlordStart";
+        const string GameLoadMethod = "BannerlordGameLoad";
+        const string TickMethod = "BannerlordTick";
+        const string ModuleUnloadMethod = "BannerlordModuleUnload";
+
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            LoadDlls();
+            InvokeMethodForAllDlls(ModuleLoadMethod);
+        }
+
+        public override void BeginGameStart(Game game)
+        {
+            base.BeginGameStart(game);
+            InvokeMethodForAllDlls(GameStartMethod, game);
+        }
+
+        public override void OnGameLoaded(Game game, object initializerObject)
+        {
+            base.OnGameLoaded(game, initializerObject);
+            InvokeMethodForAllDlls(GameLoadMethod, game, initializerObject);
+        }
+
+        protected override void OnApplicationTick(float dt)
+        {
+            base.OnApplicationTick(dt);
+            InvokeMethodForAllDlls(TickMethod, dt);
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+            InvokeMethodForAllDlls(ModuleUnloadMethod);
+        }
+
+
+        private void InvokeMethodForAllDlls(string methodName, params object[] parameters) // Public static methods only
+        {
+            foreach (DLL dll in dlls)
+            {
+                dll.InvokeMethod(methodName, BindingFlags.Public | BindingFlags.Static, parameters);
+            }
+        }
+
+        private void LoadDlls()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(Environment.CurrentDirectory); // Bannerlord exe file location
+            DLL.Directory = Path.Combine(directoryInfo.Parent.Parent.FullName, "Scripts");
+
+            // TODO: Add support for custom extensions
+            foreach (string dllPath in Directory.EnumerateFiles(DLL.Directory, "*.dll", SearchOption.AllDirectories))
+            {
+                string dllName = Path.GetFileName(dllPath);
+
+                DLL dll = new DLL(dllPath);
+                if (dll.isValid)
+                {
+                    dlls.Add(dll);
+                }
+
+            }
+        }
+
+        public static void SendMessage(string msg)
+        {
+            InformationManager.DisplayMessage(new InformationMessage(msg));
+        }
+
+        public static void SendMessage(string msg, Color color)
+        {
+            InformationManager.DisplayMessage(new InformationMessage(msg, color));
+        }
+
+        public static void ShowMessageBox(string text, MessageBoxIcon icon)
+        {
+            MessageBox.Show(text, "Yasin's DLL Loader", MessageBoxButtons.OK, icon);
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<FileInfo> GetFilesByExtensions(this DirectoryInfo dirInfo, params string[] extensions)
+        {
+            var allowedExtensions = new HashSet<string>(extensions, StringComparer.OrdinalIgnoreCase);
+
+            return dirInfo.EnumerateFiles()
+                          .Where(f => allowedExtensions.Contains(f.Extension));
+        }
+    }
+}
